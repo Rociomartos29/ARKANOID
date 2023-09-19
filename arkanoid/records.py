aaa2a406e99c11598c7f498854800d77ab37d21c
Rocío <rocionintendo29@gmail.com>
2023-09-19 22:08:39 +0200

    preparamos el inicio y el reset

--- arkanoid/records.py
+++ arkanoid/records.py
@@ -5,13 +5,21 @@ MAX_RECORDS = 10
 
 class Records:
     filename = 'records.csv'
-    base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    base_dir = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(base_dir, 'data', filename)
     def __init__(self) :
-        pass
+        self.game_records = []
+        self.data_path = os.path.join(os.path.dirname(self.path), 'data',  )
+        self.file_path = os.path.join(self.data_path, self.filename)
+        self.check_records_file()
 
     def check_records_file(self):
-        pass
+        if not os.path.isdir(self.data_path):
+            os.makedirs(self.data_path)
+        if not os.path.exists(self.file_path):
+            self.reset()
+
+        
 
     def insertar_records(self,nombre, puntuacion):
         pass
@@ -28,4 +36,7 @@ class Records:
 
 
     def reset(self):
-        pass
+        self.game_records = []
+        for cont in range(MAX_RECORDS):
+            self.game_records.append(['-----', 0])
+        self.guardar()
